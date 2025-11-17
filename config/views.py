59fd96d339001f62f99793d8cacb5e55f796a8aa
EmanuelMihai98 <mihai.degeratu98@gmail.com>
2025-11-17 22:51:40 +0200

    debuging error 500 populating db

--- config/views.py
+++ config/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib import messages
+from Profile.models import Profile
 
 def home_view(request):
     return render(request, "home.html")
 
+
 def demo_login(request):
     try:
         demo_user = User.objects.get(username="demo")
-        login(request, demo_user)
-        messages.success(request, "You're now viewing as demo user. Explore the app!")
-        return redirect("/feed/")
-    
     except User.DoesNotExist:
-        messages.error(request, "Demo user not found. Please contact the admin")
-        return redirect("/")
+        
+        demo_user = User.objects.create_user(
+            username="demo",
+            email="demo@test.com",
+            password="demo123",
+            first_name="Demo",
+            last_name="User"
+        )
+    
+        Profile.objects.get_or_create(
+            user=demo_user,
+            defaults={'bio': 'Demo user - Try the app! 🎉'}
+        )
+        messages.info(request, "Demo user created!")
+    
+    login(request, demo_user, backend='django.contrib.auth.backends.ModelBackend')
+    messages.success(request, "You're now viewing as demo user. Explore the app!")
+    return redirect("/feed/")
